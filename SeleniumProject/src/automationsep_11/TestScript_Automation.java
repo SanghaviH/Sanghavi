21d8efffc9a7db821ecbaaabc78f24ef69c1fcae
Sanghavi <sanghavihkpujar@gmail.com>
2025-09-12 07:46:41 +0530

    added multiple statement

--- SeleniumProject/src/automationsep_11/TestScript_Automation.java
+++ SeleniumProject/src/automationsep_11/TestScript_Automation.java
@@ -46,8 +46,10 @@ public class TestScript_Automation {
                     && oBrowser.getTitle().equals("actiTIME - Login")
                     && oBrowser.findElement(By.xpath("//img[contains(@src, 'timer.png')]")).isDisplayed()){
                 System.out.println("logout from actiTime is successful");
+                System.out.println("--------------------------------");
             }else{
                 System.out.println("Failed to logout from actiTime");
+                System.out.println("___________________________________________");
             }
 
         }catch (Exception e) {
